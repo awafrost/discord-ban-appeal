bc2b5d04098c89dcc8c1268022924e2800b691b2
frostslack <elhad@kimcafe.fr>
2024-08-25 19:20:23 +0200

    Update Success.js

--- src/Components/Success.js
+++ src/Components/Success.js
@@ -5,8 +5,8 @@ class Success extends Component {
     render() {
         return (
             <Grid item>
-                <h1 style={{textAlign: "center", color: "#00e676"}}>Success! Your ban appeal has been submitted to the mods!</h1>
-                <h4>Please allow some time for them to review your appeal. Abusing this system will result in a perma-ban.</h4>
+                <h1 style={{textAlign: "center", color: "#00e676"}}>Succès ! Votre demande de débanissement a été soumise aux modérateurs !</h1>
+                <h4>Veuillez leur accorder un peu de temps pour examiner votre demande. Abuser de ce système entraînera un bannissement permanent.</h4>
             </Grid>
         );
     }
